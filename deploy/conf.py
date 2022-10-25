91f59d44d023361afffb4ba1429465ac44bb7c1f
Mauricio Moldes Quaresma <MauricioMoldes@users.noreply.github.com>
2022-10-25 13:45:28 +0200

    Update conf.py database host to mongo

--- deploy/conf.py
+++ deploy/conf.py
@@ -74,7 +74,7 @@ beacon_handovers = [
 #
 # Database connection
 #
-database_host = '127.0.0.1'
+database_host = 'mongo'
 database_port = 27017
 database_user = 'root'
 database_password = 'example'
